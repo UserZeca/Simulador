bcfbfb6957b86995eb0514ae53bae576c384fa49
Ezequias Kluyvert de Oliveira Lemos <kluyvertoliveira@gmail.com>
2024-05-14 17:41:57 -0300

    fix: main - Ajustando main para conter confronto possível.

--- principal/main.cpp
+++ principal/main.cpp
@@ -28,7 +28,7 @@ int main()
 
         /* Armas de Defesa */
         ArmaDefesa* barraDeDolokohs = new BarraDeDalokohs("Um delicioso chocolate russo, envolto em mistério e poder regenerativo.", 100);
-        ArmaDefesa* descobridorDosSeteMares = new DescobridordosSeteMares("Um lança-foguetes adornado com temas náuticos.",112);
+        ArmaDefesa* descobridorDosSeteMares = new DescobridordosSeteMares("Um lança-foguetes adornado com temas náuticos.",60);
         ArmaDefesa* escudoEsplendido = new EscudoEsplendido("Um escudo majestoso, emanando uma aura de grandeza e proteção.",90);
         ArmaDefesa* sandviche = new Sandviche("Um lanche generoso e reconfortante, envolto em papel alumínio e repleto de pedaços suculentos de carne e verduras frescas.",50);
         ArmaDefesa* tarjaDeInvestida = new TarjaDeInvestida("Um escudo adornado com símbolos de investida, emitindo uma aura de determinação e agressividade.",70);
@@ -55,23 +55,26 @@ int main()
         Personagem* heavyRed = new Heavy(1, "Heavy vermelho", 300, avacalhadora5000, sandviche);
         Personagem* pyroRed = new Pyro(1, "Pyro vermelho", 150, lancarcoIris, barraDeDolokohs);
         Personagem* soldierRed = new Soldier(1, "Soldir vermelho", 200, justicaVingadora, tarjaDeInvestida);
-        Personagem* scoutRed = new Scout(1, "Scout vermelho", 150, refrisgarda, descobridorDosSeteMares);
+        Personagem* scoutRed = new Scout(1, "Scout vermelho", 150, refrisgarda, escudoEsplendido);
 
 
-        Personagem* chavesBlue = new Chaves(2, "Chaves Azul", 100, lancarcoIris, tarjaDeInvestida);
-        Personagem* demomanBlue = new Demoman(2, "Demoman Azul", 200, justicaVingadora, sandviche);
-        Personagem* pyroBlue = new Pyro(2, "Pyro Azul", 150, colherDePata, escudoEsplendido);
+        Personagem* chavesBlue = new Chaves(2, "Chaves Azul", 150, lancarcoIris, tarjaDeInvestida);
+        Personagem* demomanBlue = new Demoman(2, "Demoman Azul", 200, justicaVingadora, descobridorDosSeteMares);
+        Personagem* pyroBlue = new Pyro(2, "Pyro Azul", 150, refrisgarda, sandviche);
         Personagem* soldierBlue = new Soldier(2, "Soldir Azul", 200, pavioCurto, escudo);
 
         Simulador* simulador = new Simulador();
-       
+        
+        // Time Vermelho
         simulador->adicionarPersonagem(heavyRed, 1);
         simulador->adicionarPersonagem(pyroRed, 1);
+        simulador->adicionarPersonagem(soldierRed, 1);
         simulador->adicionarPersonagem(scoutRed, 1);
 
         // Time Azul
         simulador->adicionarPersonagem(chavesBlue, 2);
-        simulador->adicionarPersonagem(demomanBlue, 2);    
+        simulador->adicionarPersonagem(demomanBlue, 2);
+        simulador->adicionarPersonagem(pyroBlue, 2);
         simulador->adicionarPersonagem(soldierBlue, 2);
 
         simulador->iniciarSimulacao();
